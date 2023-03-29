32efd2779092f53a767e506170cc8ec7ef15ca4e
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-03-28 22:48:59 -0300

    Summarized function to the division of the values

--- calcs.asm
+++ calcs.asm
@@ -1,15 +1,8 @@
 section .text
-  global calcPorcen
 
-calcPorcen: 
-  
-  movq xmm1, xmm0 ; Copia o valor da gasolina para xmm1
-  divsd xmm1, xmm0 ; Divide o valor do etanol pelo da gasolina
+global calcPorcen
 
-  movsd xmm0, [percent] ; Carrega a constante 100.0 em xmm0
-  mulsd xmm1, xmm0 ; Multiplica o resultado pelo valor da constante
-
-  movq rax, xmm1 ; Move o resultado para o registrador de retorno
-  ret
-
-  percent: dq 100.0 ; Constante para calculo de porcentagem
+calcPorcen:  
+ divsd xmm1, xmm0 ; Divide o valor do etanol pelo da gasolina
+ movsd xmm0, xmm1 ; Move o resultado da divisão para xmm0
+ ret 
